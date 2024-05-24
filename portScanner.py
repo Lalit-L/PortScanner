0b790f677badccbe16f7dab9735a3a010a64e9fb
Lalit-L <lalitlakamsani@gmail.com>
2024-05-23 19:21:34 -0700

    Fixed initial code with target address and time

--- portScanner.py
+++ portScanner.py
@@ -2,21 +2,8 @@ import pyfiglet
 import sys
 import socket
 from datetime import datetime
-"""  
-ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
-print(ascii_banner)
-  
-# Defining a target
-if len(sys.argv) == 2:
-     
-    # translate hostname to IPv4
-    target = socket.gethostbyname(sys.argv[1]) 
-else:
-    print("Invalid amount of Argument")
-"""
 
 target = "127.0.0.1"
-# Add Banner 
 print("-" * 50)
 print("Scanning Target: " + target)
 print("Scanning started at:" + str(datetime.now()))
