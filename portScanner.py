cb83800741735cc6e4be65df68115c24c9343fcc
Lalit-L <lalitlakamsani@gmail.com>
2024-05-24 16:30:53 -0700

    Code now scans up to port 20000 on localhost, checks for open ports

--- portScanner.py
+++ portScanner.py
@@ -8,3 +8,25 @@ print("-" * 50)
 print("Scanning Target: " + target)
 print("Scanning started at:" + str(datetime.now()))
 print("-" * 50)
+
+try:
+     
+    for port in range(1,20000):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print("Port {} is open".format(port))
+        s.close()
+         
+except KeyboardInterrupt:
+        print("\n Exiting Program !!!!")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+except socket.error:
+        print("\ Server not responding !!!!")
+        sys.exit()
+
